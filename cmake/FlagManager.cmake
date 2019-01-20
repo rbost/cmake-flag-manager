d760a2546953eb022ce1e4966bb7c88b94fc98f8
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-01-20 11:09:11 +0100

    Add optional arguments to specify C++ only flags and PUBLIC options. Using the CXX_ONLY options reduces the configuration by avoiding to test flags that are knows to be restricted to C++ on a C compiler. The PUBLIC option of target_apply_saved_options makes that the saved options are set as PUBLIC options by target_compile_options (PRIVATE by default)

--- cmake/FlagManager.cmake
+++ cmake/FlagManager.cmake
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 cmake_minimum_required(VERSION 3.0)
 
 if(__flag_manager)
@@ -28,19 +27,38 @@ if(__flag_manager)
 endif()
 set(__flag_manager YES)
 
+if(${CMAKE_VERSION} VERSION_LESS "3.5.0")
+    include(CMakeParseArguments)
+endif()
+
 include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
 
 function(save_compile_option)
-    foreach(_flag ${ARGN})
+
+    set(options CXX_ONLY)
+    set(oneValueArgs)
+    set(multiValueArgs)
+
+    cmake_parse_arguments(
+        ARGUMENTS
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
+
+    foreach(_flag ${ARGUMENTS_UNPARSED_ARGUMENTS})
 
         set(FLAG_VARNAME "COMPILE_FLAG${_flag}_SUPPORTED")
         set(CFLAG_VARNAME "C_${FLAG_VARNAME}")
         set(CXXFLAG_VARNAME "CXX_${FLAG_VARNAME}")
 
-        check_c_compiler_flag(${_flag} ${CFLAG_VARNAME})
-        if(${CFLAG_VARNAME})
-            list(APPEND SAVED_CFLAG_LIST ${_flag})
+        if(NOT ${ARGUMENTS_CXX_ONLY})
+            check_c_compiler_flag(${_flag} ${CFLAG_VARNAME})
+            if(${CFLAG_VARNAME})
+                list(APPEND SAVED_CFLAG_LIST ${_flag})
+            endif()
         endif()
 
         check_cxx_compiler_flag(${_flag} ${CXXFLAG_VARNAME})
@@ -55,18 +73,42 @@ function(save_compile_option)
 endfunction(save_compile_option)
 
 function(target_apply_saved_options)
-    # message(STATUS "C compiler flags: ${SAVED_CFLAG_LIST}")
+    set(options PUBLIC)
+    set(oneValueArgs)
+    set(multiValueArgs)
 
-    # message(STATUS "C++ compiler flags: ${SAVED_CXXFLAG_LIST}")
-    foreach(_target ${ARGN})
+    cmake_parse_arguments(
+        ARGUMENTS
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
 
-        target_compile_options(
-            ${_target}
-            PRIVATE $<$<COMPILE_LANGUAGE:C>:${SAVED_CFLAG_LIST}>
-        )
-        target_compile_options(
-            ${_target}
-            PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${SAVED_CXXFLAG_LIST}>
-        )
+    message(STATUS "C compiler flags: ${SAVED_CFLAG_LIST}")
+
+    message(STATUS "C++ compiler flags: ${SAVED_CXXFLAG_LIST}")
+    foreach(_target ${ARGUMENTS_UNPARSED_ARGUMENTS})
+
+        if(${ARGUMENTS_PUBLIC} EQUAL TRUE)
+            target_compile_options(
+                ${_target}
+                PUBLIC $<$<COMPILE_LANGUAGE:C>:${SAVED_CFLAG_LIST}>
+            )
+            target_compile_options(
+                ${_target}
+                PUBLIC $<$<COMPILE_LANGUAGE:CXX>:${SAVED_CXXFLAG_LIST}>
+            )
+        else()
+            target_compile_options(
+                ${_target}
+                PRIVATE $<$<COMPILE_LANGUAGE:C>:${SAVED_CFLAG_LIST}>
+            )
+            target_compile_options(
+                ${_target}
+                PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${SAVED_CXXFLAG_LIST}>
+            )
+        endif()
+
     endforeach()
 endfunction(target_apply_saved_options)
